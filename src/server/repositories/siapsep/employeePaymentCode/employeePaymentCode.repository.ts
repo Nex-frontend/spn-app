5eb13ed8f4dd5fc939d94f32f0741bab84506391
Nex-frontend <odraude28@live.com.mx>
2026-03-31 13:52:33 -0600

    Qna_fin query

--- src/server/repositories/siapsep/employeePaymentCode/employeePaymentCode.repository.ts
+++ src/server/repositories/siapsep/employeePaymentCode/employeePaymentCode.repository.ts
@@ -2,10 +2,9 @@ import { db } from "~/server/db"
 import { EmployeePaymentCodeI } from "./employeePaymentCode.interface"
 
 
-export const getManyByRFC = async (rfc: string) => {
+export const getManyByRFCANDQNA_FIN = async (rfc: string, qna_fin: string) => {
     return await db.siapsep.execute<EmployeePaymentCodeI>({
         query: 'SELECT * FROM emp_plaza WHERE rfc LIKE ? AND qna_fin LIKE ?',
-        args: [`${rfc}%`]
+        args: [`${rfc}%`, `${qna_fin}%`]
     })
 }
-
